7085a66b3cfd6506dc409aac431b57f2c746841a
Jilliann Divozzo (umich) <jillianndivozzoumich@Jillianns-MacBook-Pro.local>
2025-03-29 15:14:38 -0400

    test

--- final-project.py
+++ final-project.py
@@ -1,5 +1,5 @@
 # Your name: Jilliann and Cassidy
-# Your student id: 
-# Your email: 
+# Your student id: 41643470
+# Your email: jdivozzo@umich.edu
 # List who or what you worked with on this homework: N/A
 # If you used Generative AI, say that you used it and also how you used it.
