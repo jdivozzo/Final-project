e969f78ab6b8fc4fe719e52b6d390a19f698f32f
jdivozzo <jdivozzo@umich.edu>
2025-03-29 15:09:11 -0400

    Create final-project.py

---
+++ final-project.py
@@ -0,0 +1,5 @@
+# Your name: Jilliann and Cassidy
+# Your student id: 
+# Your email: 
+# List who or what you worked with on this homework: N/A
+# If you used Generative AI, say that you used it and also how you used it.
