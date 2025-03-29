b34af9b6b33babb141f637b0e8128081c391df84
Cassidy McKenna <kittycatmckenna@gmail.com>
2025-03-29 15:23:36 -0400

    test

--- final-project.py
+++ final-project.py
@@ -1,5 +1,5 @@
 # Your name: Jilliann and Cassidy
 # Your student id: 
-# Your email: jdivozzo@umich.edu
+# Your email: jdivozzo@umich.edu and kittycatmckenna@gmail.com
 # List who or what you worked with on this homework: N/A
 # If you used Generative AI, say that you used it and also how you used it.
