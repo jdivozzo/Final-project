fc3fd3fec3bef47751681d900b8af311a38bc68a
Cassidy McKenna <kittycatmckenna@gmail.com>
2025-04-01 15:59:50 -0400

    changed name of api_key file to be used for the makeup api requests

---
+++ makeup_api.py
@@ -0,0 +1,4 @@
+#this was for other idea for now I'll leave it commented out
+#API_KEY = 'l4nZwZLVtGkZaAad0mdoxOcqm1cG0dIn'
+import requests
+base_url = 'http://makeup-api.herokuapp.com/api/v1/products.json?'
