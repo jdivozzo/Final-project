a1142576314fa791b12f3129265ac19b69067327
Cassidy McKenna <kittycatmckenna@gmail.com>
2025-04-09 19:56:45 -0400

    finished adding makeup_api to main function

--- final-project.py
+++ final-project.py
@@ -8,6 +8,8 @@
 from makeup_brands_scrape import scrape_data
 from makeup_brands_database import create_table, insert_data
 from visualizations import plot_data, get_data_from_db, clean_data
+from makeup_api import create_table_kinds_makeup, create_brand_list, get_makeup_data,create_table_makeup
+
 def main():
     create_table()  # Ensure table exists
     data = scrape_data()  # Scrape data
@@ -18,9 +20,24 @@ def main():
         print("Data successfully inserted into the database!")
     else:
         print("No data to insert.")
+
+    db_file = "brand_data.db"
+    create_table_kinds_makeup(db_file, ['Blush','Eyeliner','Bronzer','Foundation','Mascara','Eyeshadow','Lipstick','Eyebrow','Lipliner','Nail polish'])
+    brand_lst = create_brand_list('brand_list.txt')
+    makeup_data = get_makeup_data(db_file,brand_lst)
+    if len(makeup_data) > 0:
+        create_table_makeup(db_file,makeup_data)
+        print("Data successfully inserted into the database!")
+    else:
+        print("No data to insert.")
+    
+
+
     raw_data = get_data_from_db()
     cleaned_data = clean_data(raw_data)
     plot_data(cleaned_data)
+
+    
     
 
 if __name__ == "__main__":
